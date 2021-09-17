4295313afa9499899fcc4328a794b81323c69660
Pavlova Marina <marizza.mil89@gmail.com>
2021-09-17 17:08:33 +0300

    11_Protecting_Routes

--- routes/users.js
+++ routes/users.js
@@ -1,3 +1,4 @@
+const auth = require('../middleware/auth');
 const bcrypt = require('bcrypt');
 const _ = require('lodash');
 const {User, validate} = require('../models/user');
@@ -5,7 +6,7 @@ const mongoose = require('mongoose');
 const express = require('express');
 const router = express.Router();
 
-router.post('/', async (req, res) => {
+router.post('/', auth, async (req, res) => {
     const { error } = validate(req.body); 
     if (error) return res.status(400).send(error.details[0].message);
 
